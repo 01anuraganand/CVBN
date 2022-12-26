19c7d2439e0223e33acdaac64ca9c7b90ced3095
000Sushant <000sushantkumar@gmail.com>
2022-12-26 11:41:13 +0530

    frontend

--- core/forms.py
+++ core/forms.py
@@ -6,4 +6,13 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ['photo']
+        # widgets = {
+        #     'myfield': forms.TextInput(attrs={'class': 'myfieldclass'}),
+        # }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'id': 'file'
+        })
 
